9788656246ea6f7aca8c421aafc1cdc5a9f965a7
Tejas pagare <tejaspagare1625@gmail.com>
2024-11-07 02:33:08 +0530

    Added basic setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import { cn } from "@/lib/utils";
+
+
+export function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="Home">
+          
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Register">
+                  </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="About us">
+        
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
