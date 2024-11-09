8a64cbdb451e9059c9cc0faec0c9f590d27eada9
Naman Goyal <147164659+naman-0105@users.noreply.github.com>
2024-11-10 03:01:58 +0530

    Update UpcomingEvent.tsx

--- components/UpcomingEvent.tsx
+++ components/UpcomingEvent.tsx
@@ -1,38 +1,30 @@
-"use client";
-import React, { useState, useRef, useEffect } from "react";
-import { LayoutGrid } from "@/components/ui/layout-grid";
+import { HeroScrollDemo } from "@/components/BannerRegister";
+import Footer from "@/components/Footer";
+import { HeroHighlightDemo } from "@/components/HeroSection";
+import { Navbar } from "@/components/Navbar";
+import { PastEvents } from "@/components/PastEvents";
+import { UpcomingEvents } from "@/components/UpcomingEvent";
+import Image from "next/image";
 
-export function UpcomingEvents({ day }: { day: number }) {
-  const [events, setEvents] = useState<any[]>([]);
-
-  useEffect(() => {
-    fetch("/upcoming_events.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setEvents(data);
-      })
-      .catch((error) => console.error("Error fetching events data:", error));
-  }, []);
-
+export default function Home() {
   return (
-    <div className="h-screen py-20 w-full">
-      <h1 className="text-neutral-300 font-bold text-3xl px-12">DAY {day.toString()}</h1>
-      <LayoutGrid cards={events.map((event) => ({
-        id: event.id,
-        content: <EventSkeleton event={event} />, // Dynamically created content for each event
-        className: "col-span-1",
-        thumbnail: event.thumbnail,
-      }))} />
+    <div className="">
+      <Navbar />
+      <section id="hero">
+        <HeroHighlightDemo />
+      </section>
+      <section id="register">
+        <HeroScrollDemo />
+      </section>
+      <section id="upcoming-events">
+        <UpcomingEvents day={1} />
+        <UpcomingEvents day={2} />
+        <UpcomingEvents day={3} />
+      </section>
+      <section id="past-events">
+        <PastEvents />
+      </section>
+      <Footer />
     </div>
   );
 }
-
-const EventSkeleton = ({ event }: { event: any }) => {
-  return (
-    <div>
-      <p className="font-bold md:text-4xl text-xl text-white">{event.title}</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">{event.description}</p>
-    </div>
-  );
-};
